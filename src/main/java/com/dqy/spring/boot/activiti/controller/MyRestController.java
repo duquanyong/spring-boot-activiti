8058a4a65ca51cd85507f2ed8bc07a829dd066cf
duquanyong <duquan.yong@163.com>
2017-08-23 20:50:34 +0800

    2

--- src/main/java/com/dqy/spring/boot/activiti/controller/MyRestController.java
+++ src/main/java/com/dqy/spring/boot/activiti/controller/MyRestController.java
@@ -15,57 +15,60 @@ import com.dqy.spring.boot.activiti.service.MyService;
 
 public class MyRestController {
 	@Autowired
-    private MyService myService;
+	private MyService myService;
 
-    @RequestMapping(value="/process", method= RequestMethod.POST)
-    public void startProcessInstance(@RequestBody StartProcessRepresentation startProcessRepresentation) {
-        myService.startProcess(startProcessRepresentation.getAssignee());
-    }
+	@RequestMapping(value = "/process", method = RequestMethod.POST)
+	public void startProcessInstance(@RequestBody StartProcessRepresentation startProcessRepresentation) {
+		myService.startProcess(startProcessRepresentation.getAssignee());
+	}
 
-    @RequestMapping(value="/tasks", method= RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-    public List<TaskRepresentation> getTasks(@RequestParam String assignee) {
-        List<Task> tasks = myService.getTasks(assignee);
-        List<TaskRepresentation> dtos = new ArrayList<TaskRepresentation>();
-        for (Task task : tasks) {
-            dtos.add(new TaskRepresentation(task.getId(), task.getName()));
-        }
-        return dtos;
-    }
+	@RequestMapping(value = "/tasks", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<TaskRepresentation> getTasks(@RequestParam String assignee) {
+		List<Task> tasks = myService.getTasks(assignee);
+		List<TaskRepresentation> dtos = new ArrayList<TaskRepresentation>();
+		for (Task task : tasks) {
+			dtos.add(new TaskRepresentation(task.getId(), task.getName()));
+		}
+		return dtos;
+	}
 
-    static class TaskRepresentation {
+	static class TaskRepresentation {
 
-        private String id;
-        private String name;
+		private String id;
+		private String name;
 
-        public TaskRepresentation(String id, String name) {
-            this.id = id;
-            this.name = name;
-        }
+		public TaskRepresentation(String id, String name) {
+			this.id = id;
+			this.name = name;
+		}
 
-         public String getId() {
-            return id;
-        }
-        public void setId(String id) {
-            this.id = id;
-        }
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
-    }
-    
-    static class StartProcessRepresentation {
+		public String getId() {
+			return id;
+		}
 
-        private String assignee;
+		public void setId(String id) {
+			this.id = id;
+		}
 
-        public String getAssignee() {
-            return assignee;
-        }
+		public String getName() {
+			return name;
+		}
 
-        public void setAssignee(String assignee) {
-            this.assignee = assignee;
-        }
-    }
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
+
+	static class StartProcessRepresentation {
+
+		private String assignee;
+
+		public String getAssignee() {
+			return assignee;
+		}
+
+		public void setAssignee(String assignee) {
+			this.assignee = assignee;
+		}
+	}
 }
